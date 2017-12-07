9d1c131c00bc144378c77726779702eb0e6f7185
naotakak <naotakakinoshita@gmail.com>
2017-12-06 21:01:35 -0500

    ??

--- sems.c
+++ sems.c
@@ -5,34 +5,45 @@
 #include <sys/sem.h>
 #include <string.h>
 
-#define KEY 0
+#define KEY 23
 
 int main(int argc, char *argv[]) {
   int md, val;
-  md = semget(KEY, 4, IPC_CREAT | 0666);
-  if (!strcmp(argv[1], "-c")) {
-    if (argv[2]) {
-      if (md) {
-        printf("Semaphore created: %d\n", md);
-        printf("argv %s\n", *argv[2]);
-        semctl(md, 0, SETVAL, *argv[2]);
-        val = semctl(md, 0, GETVAL);
+  printf("B\n");
+  if (argc > 2) {
+    printf("A\n");
+    if (!strcmp(argv[1], "-c")) {
+      printf("H\n");
+      if (argv[2]) {
+        md = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0664);
+        if (md == -1) {
+          printf("semaphore already exists\n");
+          return 0;
+        }
+        printf("semaphore created: %d\n", md);
+        printf("argv %d\n", atoi(argv[2]));
+        semctl(md, 0, SETVAL, atoi(argv[2]));
+        //val = semctl(md, 0, GETVAL);
+        val = atoi(argv[2]);
         printf("value set: %d\n", val);
       }
       else {
-
+        printf("Missing value argument\n");
       }
     }
-    else {
-      printf("Missing value argument\n");
+    else if (!strcmp(argv[1], "-v")) {
+      md = semget(KEY, 1, 0664);
+      val = semctl(md, 0, GETVAL);
+      printf("semaphore value: %d\n", val);
     }
-  }
-  else if (!strcmp(argv[1], "-v")) {
-    val = semctl(md, 0, GETVAL);
-    printf("semaphore value: %d\n", val);
-  }
-  else if (!strcmp(argv[1], "-r")) {
-
+    else if (!strcmp(argv[1], "-r")) {
+      md = semget(KEY, 1, 0664);
+      val = semctl(md, 0, IPC_RMID);
+      printf("semaphore removed: %d\n", val);
+    }
+    else {
+      printf("Incorrect arguments\n");
+    }
   }
   return 0;
 }
